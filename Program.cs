03c156b418b36f6d10e39b8ed73e25f6ec3266bf
VincentKiwi <vincent.kiwanuka@elev.ga.ntig.se>
2023-08-30 13:17:51 +0200

    något första

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("vad heter du?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("en sport du gillar?");
+
+string namr = Console.ReadLine();
+
+Console.WriteLine("är det sant ");
+
+string namt = Console.ReadLine();
+
+Console.WriteLine($"{name} är sämst på {namr} är det sant {namt}");
+
+Console.ReadLine();
+
+
